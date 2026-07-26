81026152ddffdf33eb9207cb3d8f20e461822659
SEBASTIAN <sebastian.chicata@gmail.com>
2026-07-25 21:58:01 -0500

    Primer avance del Use-Store

---
+++ src/auth/controller.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from "express";
+
+export class AuthController {
+
+    //DI
+    constructor(){}
+
+    registerUser = (req: Request, res: Response) => {
+
+        res.json('RegisterUser')
+
+    }
+
+    loginUser = (req: Request, res: Response) => {
+
+        res.json('LoginUser')
+
+    }
+
+    validateEmail = (req: Request, res: Response) => {
+
+        res.json('ValidateEmail')
+
+    }
+
+}
